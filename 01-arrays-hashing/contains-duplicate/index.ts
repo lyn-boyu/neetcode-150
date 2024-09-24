3a2febb9aad61cf4f9780dd8fe16208553a54592
jason-l <linc.3395@gmail.com>
2024-09-24 09:49:58 -0700

    feat: add solution for problems/duplicate-integer

--- 01-arrays-hashing/contains-duplicate/index.ts
+++ 01-arrays-hashing/contains-duplicate/index.ts
@@ -24,6 +24,14 @@ export class Solution {
      * @return {boolean}
      */
     hasDuplicate(nums: number[]): boolean {
+        const set = new Set()
+        for (let num of nums) {
+            if (set.has(num)) {
+                return true
+            } else {
+                set.add(num)
+            }
+        }
         return false
     }
 }
