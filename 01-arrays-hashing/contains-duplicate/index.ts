65856642fc65dcad4b2e38a805c4ccbd40612055
jason-l <linc.3395@gmail.com>
2024-09-24 09:57:44 -0700

    feat: add problem contains-duplicate

---
+++ 01-arrays-hashing/contains-duplicate/index.ts
@@ -0,0 +1,29 @@
+
+/**
+
+Duplicate Integer - Easy
+
+https://neetcode.io/problems/duplicate-integer
+
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+Input: nums = [1, 2, 3, 3]
+Output: true
+
+Example 2:
+Input: nums = [1, 2, 3, 4]
+Output: false
+
+ */
+
+
+export class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        return false
+    }
+}
