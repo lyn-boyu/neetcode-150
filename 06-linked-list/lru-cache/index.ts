3ddac1c06247b495383c4e27b1e98824bb94522f
jason-l <linc.3395@gmail.com>
2024-09-26 13:49:02 -0700

    feat: add solution for lru cache

--- 06-linked-list/lru-cache/index.ts
+++ 06-linked-list/lru-cache/index.ts
@@ -38,21 +38,115 @@
  * - 0 <= value <= 1000
  */
 
+
+class Node {
+    key: number
+    value: number
+    next: Node | null
+    prev: Node | null
+    constructor(key: number, value: number) {
+        this.key = key
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
 class LRUCache {
     private capacity: number;
-    private cache: Map<number, number>;
+    private cache: Map<number, Node>;
+    private head: Node | null
+    // put exceed capacity will use tail to remove 
+    private tail: Node | null
 
     constructor(capacity: number) {
         this.capacity = capacity;
-        this.cache = new Map();
+        this.cache = new Map<number, Node>();
+        this.head = null
+        this.tail = null
     }
 
+    private removeNode(node: Node) {
+        // remove node from  prev node
+        if (node.prev) {
+            node.prev.next = node.next
+        } else {
+            // if it is head node update this.head
+            this.head = node.next
+        }
+
+        if (node.next) {
+            // disconnect with next node 
+            node.next.prev = node.prev
+        } else {
+            // if it is tail node update this.tail
+            this.tail = node.prev
+        }
+
+    }
+
+    private insertAtFront(node: Node) {
+        // update node next and prev
+        node.next = this.head
+        node.prev = null
+        // update head
+        if (this.head) {
+            this.head.prev = node
+        }
+        this.head = node
+
+        // update if there is no tail
+        if (!this.tail) {
+            this.tail = node
+        }
+
+    }
+
+    // return -1 if key is not exist in the cache
     get(key: number): number {
-        return -1
+        // find node in map return -1 if not exist 
+        if (!this.cache.has(key)) return -1
+
+        const node = this.cache.get(key)!;
+        // update linked list
+        // - remove node from list
+        this.removeNode(node)
+        // - move node to head 
+        this.insertAtFront(node)
+
+        // return value
+        return node.value
     }
 
     put(key: number, value: number): void {
-
+        if (this.cache.has(key)) {
+            // if cache exist same key 
+            const node = this.cache.get(key)!;
+            // delete node from list
+            this.removeNode(node)
+            // update value
+            node.value = value
+            // insert node atFront
+            this.insertAtFront(node)
+        } else {
+            const newNode = new Node(key, value)
+            // if it is a new node
+            if (this.cache.size >= this.capacity) {
+                // case1: the capacity is exceeded
+                // - remove the tail node from list
+                // - remove the tail node from the map
+                if (this.tail) {
+                    // delete cache first
+                    this.cache.delete(this.tail?.key)
+                    this.removeNode(this.tail)
+                }
+            }
+            // case2: the capacity is not exceed
+            // - add to map
+            this.cache.set(key, newNode)
+            // - add to front
+            this.insertAtFront(newNode)
+        }
     }
 }
 
