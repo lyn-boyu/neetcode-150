f37473996dc08e796e427eefe0c80ecb78dd881f
jason-l <linc.3395@gmail.com>
2024-09-27 16:30:21 -0700

    feat: add solution for problems/validate-parentheses

--- 04-stack/validate-parentheses/index.ts
+++ 04-stack/validate-parentheses/index.ts
@@ -35,5 +35,26 @@
  */
 
 export function isValid(s: string): boolean {
-    return false
+    const stack = []
+    const bracketPairs: Record<string, string> = {
+        ')': '(',
+        ']': '[',
+        '}': '{',
+    }
+
+    const closeBrackets = Object.keys(bracketPairs)
+    const openBrackets = Object.values(bracketPairs)
+
+    for (let char of s) {
+        if (openBrackets.includes(char)) {
+            stack.push(char)
+        } else if (closeBrackets.includes(char)) {
+            if (stack.length === 0 || stack.pop() !== bracketPairs[char]) {
+                return false
+            }
+        }
+    }
+
+    // Ensure no unclosed brackets remain
+    return stack.length === 0
 }
