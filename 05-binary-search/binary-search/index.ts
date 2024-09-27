cff582097e1147880454a2903529c5c45de8744f
jason-l <linc.3395@gmail.com>
2024-09-27 10:55:42 -0700

    feat: add solution for binary-search

--- 05-binary-search/binary-search/index.ts
+++ 05-binary-search/binary-search/index.ts
@@ -23,5 +23,21 @@
  * - -10000 <= nums[i], target <= 10000
  */
 export function binarySearch(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1
+    // need comparation when left and right are equal
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2)
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] > target) {
+            // move search to left part
+            right = mid - 1
+        } else {
+            // move search to right part
+            left = mid + 1
+        }
+    }
+
     return -1; // target not found
 }
