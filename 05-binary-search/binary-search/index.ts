9e5f720a489e11bf5ab00329e4332f59bdda7da9
jason-l <linc.3395@gmail.com>
2024-09-27 10:49:11 -0700

    feat: add problem binary search

---
+++ 05-binary-search/binary-search/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Binary Search - Easy
+ * 
+ * https://neetcode.io/problems/binary-search
+ * 
+ * You are given an array of distinct integers `nums`, sorted in ascending order, and an integer `target`.
+ * Implement a function to search for `target` within `nums`. If it exists, then return its index, otherwise return `-1`.
+ * 
+ * Your solution must run in O(logn) time.
+ * 
+ * Example 1:
+ * Input: nums = [-1, 0, 2, 4, 6, 8], target = 4
+ * Output: 3
+ * Explanation: nums[3] is 4, which matches the target.
+ * 
+ * Example 2:
+ * Input: nums = [-1, 0, 2, 4, 6, 8], target = 3
+ * Output: -1
+ * Explanation: 3 is not present in nums, so return -1.
+ * 
+ * Constraints:
+ * - 1 <= nums.length <= 10000
+ * - -10000 <= nums[i], target <= 10000
+ */
+export function binarySearch(nums: number[], target: number): number {
+    return -1; // target not found
+}
