faa8063976edfb89ff4d9c84e73f2a5f398c080f
jason-l <linc.3395@gmail.com>
2024-09-27 22:11:58 -0700

    feat: add solution for problems/two-integer-sum-ii

--- 02-two-pointers/two-integer-sum-ii/index.ts
+++ 02-two-pointers/two-integer-sum-ii/index.ts
@@ -30,5 +30,19 @@
  */
 
 export function twoSum(numbers: number[], target: number): number[] {
+    let left = 0
+    let right = numbers.length - 1;
+
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+        if (sum === target) {
+            return [left + 1, right + 1]
+        } else if (sum < target) {
+            left++
+        } else {
+            right--
+        }
+    }
+
     return []; // This line should never be reached due to the assumption that there is always a valid solution
 }
