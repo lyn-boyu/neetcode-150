d7b18c0626a38785a8a06dc6d9cbef284e610085
jason-l <linc.3395@gmail.com>
2024-09-27 21:41:38 -0700

    feat: add solution for problems/longest-consecutive-sequence

--- 01-arrays-hashing/longest-consecutive-sequence/index.ts
+++ 01-arrays-hashing/longest-consecutive-sequence/index.ts
@@ -28,5 +28,28 @@
  */
 
 export function longestConsecutive(nums: number[]): number {
-    return -1
+
+    if (nums.length === 0) return 0
+
+    let maxLength = 0
+    const numSet = new Set(nums)
+
+    for (let num of numSet) {
+        if (numSet.has(num - 1)) {
+            continue
+        }
+
+        let currentNum = num
+        let currentLength = 0
+
+        while (numSet.has(currentNum)) {
+            currentLength += 1
+            currentNum += 1
+        }
+
+        maxLength = Math.max(currentLength, maxLength)
+    }
+
+
+    return maxLength
 }
