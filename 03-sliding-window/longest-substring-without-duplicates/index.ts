52eee87c1787835a016df7fbaa00033242121f0f
jason-l <linc.3395@gmail.com>
2024-09-28 13:08:09 -0700

    feat: add problems/longest-substring-without-duplicates

---
+++ 03-sliding-window/longest-substring-without-duplicates/index.ts
@@ -0,0 +1,24 @@
+/**
+ * Longest Substring Without Duplicates - Medium
+ * 
+ * https://neetcode.io/problems/longest-substring-without-duplicates
+ * 
+ * Given a string `s`, find the length of the longest substring without duplicate characters.
+ * 
+ * Example 1:
+ * Input: s = "zxyzxyz"
+ * Output: 3
+ * Explanation: The string "xyz" is the longest without duplicate characters.
+ * 
+ * Example 2:
+ * Input: s = "xxxx"
+ * Output: 1
+ * 
+ * Constraints:
+ * - 0 <= s.length <= 1000
+ * - s may consist of printable ASCII characters.
+ */
+
+export function lengthOfLongestSubstring(s: string): number {
+    return -1
+}
