af5acb58822788daa95a798db9255991ee051136
jason-l <linc.3395@gmail.com>
2024-09-27 18:50:14 -0700

    feat: add  problems/generate-parentheses

---
+++ 04-stack/generate-parentheses/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Generate Parentheses - Medium
+ * 
+ * https://neetcode.io/problems/generate-parentheses
+ * 
+ * You are given an integer `n`. Return all well-formed parentheses strings that you can generate with `n` pairs of parentheses.
+ * 
+ * Example 1:
+ * Input: n = 1
+ * Output: ["()"]
+ * 
+ * Example 2:
+ * Input: n = 3
+ * Output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
+ * 
+ * Constraints:
+ * - 1 <= n <= 7
+ * 
+ * Solution:
+ * - Use backtracking to generate all possible combinations of parentheses.
+ * - Track the number of open and closed parentheses to ensure well-formed strings.
+ */
+
+export function generateParenthesis(n: number): string[] {
+    return []
+}
