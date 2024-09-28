b7d6fb57a10c8968660b9ae1980f95c2ae69f442
jason-l <linc.3395@gmail.com>
2024-09-27 18:57:01 -0700

    feat: add solution for problems/generate-parentheses

--- 04-stack/generate-parentheses/index.ts
+++ 04-stack/generate-parentheses/index.ts
@@ -22,5 +22,24 @@
  */
 
 export function generateParenthesis(n: number): string[] {
-    return []
+    const result: string[] = []
+
+    const backtrack = (current: string, open: number, close: number) => {
+        if (current.length === 2 * n) {
+            result.push(current)
+        }
+
+        if (open < n) {
+            backtrack(current + '(', open + 1, close)
+        }
+
+        if (close < open) {
+            backtrack(current + ')', open, close + 1)
+        }
+
+    }
+
+    backtrack('', 0, 0)
+
+    return result
 }
