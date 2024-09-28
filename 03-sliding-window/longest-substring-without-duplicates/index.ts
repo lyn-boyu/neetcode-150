20e15b3ea697e93528c0a1a146754b4b7c559cea
jason-l <linc.3395@gmail.com>
2024-09-28 13:21:53 -0700

    feat: add solution for problems/longest-substring-without-duplicates

--- 03-sliding-window/longest-substring-without-duplicates/index.ts
+++ 03-sliding-window/longest-substring-without-duplicates/index.ts
@@ -20,5 +20,24 @@
  */
 
 export function lengthOfLongestSubstring(s: string): number {
-    return -1
+    let maxLength = 0
+    let left = 0
+    let charIndexMap: Record<string, number> = {}
+
+    for (let right = 0; right < s.length; right++) {
+        const char = s[right];
+        // Move the left pointer to the position after the last occurrence of the current character
+        // if it exists in the map and is within the current window
+        if (charIndexMap[char] !== undefined && charIndexMap[char] >= left) {
+            left = charIndexMap[char] + 1
+        }
+        // update char index
+        charIndexMap[char] = right
+
+        // update max length
+        const curentLength = right - left + 1
+        maxLength = Math.max(maxLength, curentLength)
+    }
+
+    return maxLength
 }
