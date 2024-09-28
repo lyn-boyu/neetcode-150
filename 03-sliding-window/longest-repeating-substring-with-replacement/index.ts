561438933e6d781b5297d85ab741ee56fb4763c2
jason-l <linc.3395@gmail.com>
2024-09-28 13:29:00 -0700

    feat: add problems/longest-repeating-substring-with-replacement

---
+++ 03-sliding-window/longest-repeating-substring-with-replacement/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Longest Repeating Substring With Replacement - Medium
+ * 
+ * https://neetcode.io/problems/longest-repeating-substring-with-replacement
+ * 
+ * You are given a string `s` consisting of only uppercase English characters and an integer `k`.
+ * You can choose up to `k` characters of the string and replace them with any other uppercase English character.
+ * After performing at most `k` replacements, return the length of the longest substring which contains only one distinct character.
+ * 
+ * Example 1:
+ * Input: s = "XYYX", k = 2
+ * Output: 4
+ * Explanation: Either replace the 'X's with 'Y's, or replace the 'Y's with 'X's.
+ * 
+ * Example 2:
+ * Input: s = "AAABABB", k = 1
+ * Output: 5
+ * 
+ * Constraints:
+ * - 1 <= s.length <= 1000
+ * - 0 <= k <= s.length
+ */
+
+export function characterReplacement(s: string, k: number): number {
+     return -1
+}
+
