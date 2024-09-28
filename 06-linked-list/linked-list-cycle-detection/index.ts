35cac96983219cee47bae27e28e2d177862e01de
jason-l <linc.3395@gmail.com>
2024-09-27 22:27:55 -0700

    feat: add problems/linked-list-cycle-detection

--- 06-linked-list/linked-list-cycle-detection/index.ts
+++ 06-linked-list/linked-list-cycle-detection/index.ts
@@ -37,6 +37,20 @@ class ListNode {
 }
 
 export function hasCycle(head: ListNode | null): boolean {
+
+    let slow = head
+    let fast = head
+
+    // loop end when fast node reach the end
+    while (fast !== null && fast!.next !== null) {
+        slow = slow!.next
+        fast = fast!.next.next
+
+        if (slow === fast) {
+            return true
+        }
+    }
+
     return false
 }
 
