18b7d3834c44e42a06da41922ecd8b3951854c60
jason-l <linc.3395@gmail.com>
2024-09-28 13:02:07 -0700

    feat: add solution for problems/longest-repeating-substring-with-replacement

--- 03-sliding-window/longest-repeating-substring-with-replacement/index.ts
+++ 03-sliding-window/longest-repeating-substring-with-replacement/index.ts
@@ -22,6 +22,27 @@
  */
 
 export function characterReplacement(s: string, k: number): number {
-     return -1
+    let left = 0;
+    let maxCount = 0;
+    let maxLength = 0;
+
+    const freqMap: Record<string, number> = {}
+
+    for (let right = 0; right < s.length; right++) {
+        const char = s[right]
+        freqMap[char] = (freqMap[char] ?? 0) + 1;
+        maxCount = Math.max(freqMap[char], maxCount)
+
+        // move left when replace count is bigger than k
+        if (right - left + 1 - maxCount > k) {
+            const leftChar = s[left]
+            freqMap[leftChar] -= 1
+            left += 1
+        }
+
+        maxLength = Math.max(maxLength, right - left + 1)
+    }
+
+    return maxLength
 }
 
