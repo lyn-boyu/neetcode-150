54dbdc64b07a85205ef36f4b5d183b6e34b57b57
jason-l <linc.3395@gmail.com>
2024-09-29 01:22:00 -0700

    feat: add solution for problems/meeting-schedule

--- 16-intervals/meeting-schedule/index.ts
+++ 16-intervals/meeting-schedule/index.ts
@@ -25,5 +25,17 @@
  */
 
 export function canAttendAllMeetings(intervals: [number, number][]): boolean {
+    // sort intervals by the start time in ascending way
+    intervals.sort((a, b) => a[0] - b[0])
+    for (let i = 1; i < intervals.length; i++) {
+
+        const currentStartTime = intervals[i][0]
+        const lastEndTime = intervals[i - 1][1]
+        // confilcts means 
+        if (currentStartTime < lastEndTime) {
+            return false
+        }
+    }
+
     return true;
 }
