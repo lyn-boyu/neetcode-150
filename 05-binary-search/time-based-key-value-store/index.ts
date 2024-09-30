7238f79e50a14c013f82d603d2e543a56e37e931
jason-l <linc.3395@gmail.com>
2024-09-30 01:40:32 -0700

    feat: add solution for problems/time-based-key-value-store

--- 05-binary-search/time-based-key-value-store/index.ts
+++ 05-binary-search/time-based-key-value-store/index.ts
@@ -45,10 +45,32 @@ export class TimeMap {
     }
 
     set(key: string, value: string, timestamp: number): void {
-
+        if (!this.store.has(key)) {
+            this.store.set(key, [])
+        }
+        this.store.get(key)!.push({ timestamp, value })
     }
 
     get(key: string, timestamp: number): string {
-       return ''
+        if (!this.store.has(key)) return ''
+
+        const values = this.store.get(key) ?? []
+        let left = 0
+        let right = values?.length - 1
+        let res = ''
+
+        while (left <= right) {
+            const mid = Math.floor((right + left) / 2);
+
+            if (values[mid].timestamp <= timestamp) {
+                res = values[mid].value
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+
+        }
+
+        return res
     }
 }
