6d917bba97260fd15d053aa6d748c7a88fe1fedf
jason-l <linc.3395@gmail.com>
2024-09-30 10:59:41 -0700

    feat: add solution for problems/find-target-in-rotated-sorted-array

--- 05-binary-search/find-target-in-rotated-sorted-array/index.ts
+++ 05-binary-search/find-target-in-rotated-sorted-array/index.ts
@@ -33,5 +33,37 @@
  */
 
 export function search(nums: number[], target: number): number {
-    return -1; // Target not found
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        // Determine which side of the array is sorted
+        if (nums[left] <= nums[mid]) {
+            // If the left side is sorted
+            if (nums[left] <= target && target < nums[mid]) {
+                // Target is in the left sorted portion, move to the left side
+                right = mid - 1;
+            } else {
+                // Target is not in the left sorted portion, move to the right side
+                left = mid + 1;
+            }
+        } else {
+            // If the right side is sorted
+            if (nums[mid] < target && target <= nums[right]) {
+                // Target is in the right sorted portion, move to the right side
+                left = mid + 1;
+            } else {
+                // Target is not in the right sorted portion, move to the left side
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1; // If the target is not found
 }
