a16ff5a0bd2907fd8df89376b0021e570c85089a
jason-l <linc.3395@gmail.com>
2024-09-30 00:22:40 -0700

    feat: add  problems/string-encode-decode

---
+++ 01-arrays-hashing/string-encode-decode/index.ts
@@ -0,0 +1,33 @@
+/**
+ * String Encode and Decode - Medium
+ * 
+ * https://neetcode.io/problems/string-encode-decode
+ * 
+ * Design an algorithm to encode a list of strings to a single string. 
+ * The encoded string is then decoded back to the original list of strings.
+ * 
+ * Example 1:
+ * Input: ["neet","code","love","you"]
+ * Output: ["neet","code","love","you"]
+ * 
+ * Example 2:
+ * Input: ["we","say",":","yes"]
+ * Output: ["we","say",":","yes"]
+ * 
+ * Constraints:
+ * - 0 <= strs.length < 100
+ * - 0 <= strs[i].length < 200
+ * - strs[i] contains only UTF-8 characters.
+ */
+
+export class Codec {
+    // Encode a list of strings to a single string
+    encode(strs: string[]): string {
+        return ''
+    }
+
+    // Decode a single string to a list of strings
+    decode(s: string): string[] {
+        return []
+    }
+}
