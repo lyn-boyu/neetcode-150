e6d0d25f5098075e7b7793ac73104e90c0d3058d
jason-l <linc.3395@gmail.com>
2024-09-30 15:52:35 -0700

    feat: add solution for problems/find-duplicate-integer

--- 06-linked-list/find-duplicate-integer/index.ts
+++ 06-linked-list/find-duplicate-integer/index.ts
@@ -29,5 +29,27 @@
  */
 
 export function findDuplicate(nums: number[]): number {
-    return -1
+    // init two pointer
+    let slow = nums[0]
+    let fast = nums[0]
+
+    while (true) {
+
+        slow = nums[slow] // move one step each time
+        fast = nums[nums[fast]] // move two step each time
+
+        if (slow === fast) {
+            break
+        }
+    }
+
+    // move slow back to the begginning
+    slow = nums[0]
+
+    while (slow !== fast) {
+        slow = nums[slow] // move one step each time
+        fast = nums[fast] // move one step each time
+    }
+
+    return slow
 }
