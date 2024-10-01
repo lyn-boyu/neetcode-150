8c5ef700a05bf5d1e9b89c2e0428855077fb24a9
jason-l <linc.3395@gmail.com>
2024-09-30 23:43:31 -0700

    feat: add problems/invert-binary-tree

---
+++ 07-trees/invert-binary-tree/index.ts
@@ -0,0 +1,66 @@
+
+/**
+ * Invert a Binary Tree - Easy
+ * 
+ * https://neetcode.io/problems/invert-binary-tree
+ * 
+ * You are given the root of a binary tree root. Invert the binary tree and return its root.
+ * 
+ * Example 1:
+ * Input: root = [1,2,3,4,5,6,7]
+ * 
+ * Original Tree:
+ *         1
+ *        / \
+ *       2   3
+ *      / \ / \
+ *     4  5 6  7
+ * 
+ * Output: [1,3,2,7,6,5,4]
+ * 
+ * Inverted Tree:
+ *         1
+ *        / \
+ *       3   2
+ *      / \ / \
+ *     7  6 5  4
+ * 
+ * Example 2:
+ * Input: root = [3,2,1]
+ * 
+ * Original Tree:
+ *         3
+ *        / \
+ *       2   1
+ * 
+ * Output: [3,1,2]
+ * 
+ * Inverted Tree:
+ *         3
+ *        / \
+ *       1   2
+ * 
+ * Example 3:
+ * Input: root = []
+ * Output: []
+ * 
+ * Constraints:
+ * - 0 <= The number of nodes in the tree <= 100
+ * - -100 <= Node.val <= 100
+ */
+
+export class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+export function invertTree(root: TreeNode | null): TreeNode | null {
+    return null;
+}
