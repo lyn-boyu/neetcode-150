dab749b1cdc1e9c83717a0b7a288e5d79b79a951
jason-l <linc.3395@gmail.com>
2024-10-01 00:01:44 -0700

    feat: add solution for problems/depth-of-binary-tree

--- 07-trees/depth-of-binary-tree/index.ts
+++ 07-trees/depth-of-binary-tree/index.ts
@@ -41,5 +41,10 @@ export class TreeNode {
 }
 
 export function maxDepth(root: TreeNode | null): number {
-    return -1
+    if (!root) return 0;
+
+    const leftDepth = maxDepth(root.left)
+    const rightDepth = maxDepth(root.right)
+
+    return Math.max(leftDepth, rightDepth) + 1
 }
