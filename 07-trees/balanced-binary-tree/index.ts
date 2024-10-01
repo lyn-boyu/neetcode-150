65c9b471897db45f7f79d627cda8104007594f15
jason-l <linc.3395@gmail.com>
2024-10-01 11:01:06 -0700

    feat: add solution for problems/balanced-binary-tree

--- 07-trees/balanced-binary-tree/index.ts
+++ 07-trees/balanced-binary-tree/index.ts
@@ -55,5 +55,22 @@ export class TreeNode {
 }
 
 export function isBalanced(root: TreeNode | null): boolean {
-    return false
+
+    const checkHeight = (node: TreeNode | null): number => {
+        if (!node) return 0
+
+        const leftHeight = checkHeight(node.left);
+        if (leftHeight === -1) return -1
+
+        const rightHeight = checkHeight(node.right);
+        if (rightHeight === -1) return -1
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1
+    }
+
+    return checkHeight(root) !== -1
 }
