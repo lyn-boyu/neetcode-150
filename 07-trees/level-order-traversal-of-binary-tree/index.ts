cfaaa31c04f2c98e435e73c6fd50de8691520aee
jason-l <linc.3395@gmail.com>
2024-10-01 14:25:04 -0700

    feat: add solution for problems/level-order-traversal

--- 07-trees/level-order-traversal-of-binary-tree/index.ts
+++ 07-trees/level-order-traversal-of-binary-tree/index.ts
@@ -47,5 +47,35 @@ export class TreeNode {
 }
 
 export function levelOrder(root: TreeNode | null): number[][] {
-    return []
+    const result: number[][] = []
+
+    if (root === null) {
+        return result
+    }
+
+    const queue: TreeNode[] = [root]
+
+    while (queue.length > 0) {
+
+        const levelSize = queue.length;
+        const currentLevel: number[] = [];
+
+        for (let i = 0; i < levelSize; i++) {
+
+            const currentNode = queue.shift()!
+            currentLevel.push(currentNode?.val!)
+
+            if (currentNode.left) {
+                queue.push(currentNode?.left)
+            }
+
+            if (currentNode.right) {
+                queue.push(currentNode.right)
+            }
+        }
+
+        result.push(currentLevel)
+    }
+
+    return result
 }
