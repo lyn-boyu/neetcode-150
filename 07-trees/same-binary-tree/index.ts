ed27c103ad88b4dd50998d38bcdd8766aceead03
jason-l <linc.3395@gmail.com>
2024-10-01 11:21:59 -0700

    feat: add solution for problems/same-binary-tree

--- 07-trees/same-binary-tree/index.ts
+++ 07-trees/same-binary-tree/index.ts
@@ -61,5 +61,21 @@ export class TreeNode {
 }
 
 export function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-    return false
+    // Check edge case1: if both nodes are null, they are equivalent
+    if (p === null && q === null) {
+        return true;
+    }
+
+    // Check edge case2: if one of the nodes is null and the other is not, they are not equivalent
+    if (p === null || q === null) {
+        return false;
+    }
+
+    // Compare the value
+    if (p.val !== q.val) {
+        return false;
+    }
+
+    // Compare child nodes: Recursively compare the left and right child nodes of both trees
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }
