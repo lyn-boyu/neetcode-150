305334e8cd88efae0cf207d78e88585961dd9a90
jason-l <linc.3395@gmail.com>
2024-10-02 13:10:05 -0700

    feat: add problems/subsets

---
+++ 09-backtracking/subsets/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Subsets - Medium
+ * 
+ * https://neetcode.io/problems/subsets
+ * 
+ * Given an array `nums` of unique integers, return all possible subsets of `nums`.
+ * 
+ * The solution set must not contain duplicate subsets. You may return the solution in any order.
+ * 
+ * Example 1:
+ * Input: nums = [1, 2, 3]
+ * Output: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+ * 
+ * Example 2:
+ * Input: nums = [7]
+ * Output: [[], [7]]
+ * 
+ * Constraints:
+ * - 1 <= nums.length <= 10
+ * - -10 <= nums[i] <= 10
+ */
+
+
+
+export function subsets(nums: number[]): number[][] {
+    return []
+}
