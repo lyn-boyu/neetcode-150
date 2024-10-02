2b4363ee2fc5c18c2d8f6930e28e332858ecd130
jason-l <linc.3395@gmail.com>
2024-10-01 22:52:02 -0700

    feat: add solution for problems/last-stone-weight

--- 08-heap-priority-queue/last-stone-weight/index.ts
+++ 08-heap-priority-queue/last-stone-weight/index.ts
@@ -39,5 +39,21 @@
  */
 
 export function lastStoneWeight(stones: number[]): number {
-    return -1
+    while (stones.length > 0) {
+        // return the only stone if there if only one stone left 
+        if (stones.length === 1) return stones[0]
+
+        // Step 1: sort 
+        stones.sort((a, b) => a - b);
+
+        const stone1 = stones.pop()! // The heaviest stone
+        const stone2 = stones.pop()! // The second heaviest stone
+
+        // Step 2: If the two stones have different weights, push the difference back
+        if (stone1 !== stone2) {
+            stones.push(stone1 - stone2)
+        }
+    }
+    // if there is no stone left 
+    return 0
 }
