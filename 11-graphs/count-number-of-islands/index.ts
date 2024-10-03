c4c0ce27cd0878e8858c36c559dd09ca3d1248a6
jason-l <linc.3395@gmail.com>
2024-10-03 00:18:02 -0700

    feat: add problems/count-number-of-islands

---
+++ 11-graphs/count-number-of-islands/index.ts
@@ -0,0 +1,37 @@
+/**
+ * Count Number of Islands - Medium
+ * 
+ * https://neetcode.io/problems/count-number-of-islands
+ * 
+ * Given a 2D grid `grid` where '1' represents land and '0' represents water, count and return the number of islands.
+ * An island is formed by connecting adjacent lands horizontally or vertically and is surrounded by water.
+ * You may assume water is surrounding the grid (i.e., all the edges are water).
+ * 
+ * Example 1:
+ * Input: grid = [
+ *     ["0","1","1","1","0"],
+ *     ["0","1","0","1","0"],
+ *     ["1","1","0","0","0"],
+ *     ["0","0","0","0","0"]
+ *   ]
+ * Output: 1
+ * Explanation: There is only one island formed by connecting the land pieces in the first three rows.
+ * 
+ * Example 2:
+ * Input: grid = [
+ *     ["1","1","0","0","1"],
+ *     ["1","1","0","0","1"],
+ *     ["0","0","1","0","0"],
+ *     ["0","0","0","1","1"]
+ *   ]
+ * Output: 4
+ * Explanation: There are four separate islands: two on the top right, one in the middle, and one on the bottom right.
+ * 
+ * Constraints:
+ * - 1 <= grid.length, grid[i].length <= 100
+ * - grid[i][j] is '0' or '1'.
+ */
+
+export function numIslands(grid: string[][]): number {
+    return -1
+}
