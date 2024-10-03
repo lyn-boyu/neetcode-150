52a2b815d2fcc2f48b574c5767a15b1bb366f50c
jason-l <linc.3395@gmail.com>
2024-10-03 00:35:43 -0700

    feat: problems/max-area-of-island

---
+++ 11-graphs/max-area-of-island/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Max Area of Island - Medium
+ * 
+ * https://neetcode.io/problems/max-area-of-island
+ * 
+ * You are given a matrix `grid` where grid[i] is either a 0 (representing water) or 1 (representing land).
+ * An island is defined as a group of 1's connected horizontally or vertically. You may assume all four edges of the grid are surrounded by water.
+ * The area of an island is defined as the number of cells within the island.
+ * 
+ * Return the maximum area of an island in grid. If no island exists, return 0.
+ * 
+ * Example 1:
+ * 
+ * Input: grid = [
+ *   [0, 1, 1, 0, 1],
+ *   [1, 0, 1, 0, 1],
+ *   [0, 1, 1, 0, 1],
+ *   [0, 1, 0, 0, 1]
+ * ]
+ * Output: 6
+ * Explanation: The largest island has 6 cells of connected '1's.
+ * 
+ * Constraints:
+ * - 1 <= grid.length, grid[i].length <= 50
+ */
+
+export function maxAreaOfIsland(grid: number[][]): number {
+    return -1
+}
