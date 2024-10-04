ef0c76320a8e6f15d4f415fb6d13acd33485f51a
jason-l <linc.3395@gmail.com>
2024-10-04 01:48:26 -0700

    feat: add problems/coin-change-ii

---
+++ 14-2d-danamic-programing/coin-change-ii/index.ts
@@ -0,0 +1,38 @@
+/**
+ * Coin Change II - Medium
+ * 
+ * https://neetcode.io/problems/coin-change-ii
+ * 
+ * You are given an integer array `coins` representing coins of different denominations 
+ * (e.g. 1 dollar, 5 dollars, etc) and an integer `amount` representing a target amount of money.
+ * 
+ * Return the number of distinct combinations that total up to `amount`. If it's impossible 
+ * to make up the amount, return 0.
+ * 
+ * You may assume that you have an unlimited number of each coin and that each value in `coins` is unique.
+ * 
+ * Example 1:
+ * 
+ * Input: amount = 4, coins = [1,2,3]
+ * Output: 4
+ * Explanation:
+ * 1+1+1+1 = 4
+ * 1+1+2 = 4
+ * 2+2 = 4
+ * 1+3 = 4
+ * 
+ * Example 2:
+ * 
+ * Input: amount = 7, coins = [2,4]
+ * Output: 0
+ * Explanation: No valid combination can sum up to 7.
+ * 
+ * Constraints:
+ * - 1 <= coins.length <= 100
+ * - 1 <= coins[i] <= 1000
+ * - 0 <= amount <= 1000
+ */
+
+export function coinChangeII(amount: number, coins: number[]): number {
+    return 0
+}
