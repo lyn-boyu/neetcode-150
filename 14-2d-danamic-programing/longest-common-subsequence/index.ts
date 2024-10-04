4ac8e0beb63af4e2ad2a63a276027f88bf6a1a8d
jason-l <linc.3395@gmail.com>
2024-10-04 01:34:16 -0700

    feat: add solution for problems/longest-common-subsequence

--- 14-2d-danamic-programing/longest-common-subsequence/index.ts
+++ 14-2d-danamic-programing/longest-common-subsequence/index.ts
@@ -37,5 +37,25 @@
  */
 
 export function longestCommonSubsequence(text1: string, text2: string): number {
-    return 0
+    const rows = text1.length;
+    const cols = text2.length
+    // step1: define state table
+    const dp = Array.from({ length: rows + 1 }, () => Array(cols + 1).fill(0))
+
+    // step2:  fill the table by state transition function
+    for (let r = 1; r <= rows; r++) {
+        for (let c = 1; c <= cols; c++) {
+            if (text1[r - 1] === text2[c - 1]) {
+                dp[r][c] = dp[r - 1][c - 1] + 1
+            } else {
+                dp[r][c] = Math.max(
+                    dp[r][c - 1],
+                    dp[r - 1][c],
+                )
+            }
+        }
+    }
+
+    // step3: return the result
+    return dp[rows][cols]
 }
