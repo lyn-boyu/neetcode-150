72a355562c77d27f745a15e4637204b618194826
jason-l <linc.3395@gmail.com>
2024-10-25 15:36:28 -0700

    feat: add problem maximum-subarray

---
+++ 15-greedy/maximum-subarray/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Maximum Subarray - Medium
+ * https://neetcode.io/problems/maximum-subarray
+ * 
+ * Given an array of integers nums, find the subarray with the largest sum and return the sum.
+ * 
+ * A subarray is a contiguous non-empty sequence of elements within an array.
+ * 
+ * Example 1:
+ * Input: nums = [2, -3, 4, -2, 2, 1, -1, 4]
+ * Output: 8
+ * Explanation: The subarray [4, -2, 2, 1, -1, 4] has the largest sum of 8.
+ * ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+ * │  2  │ -3  │  4  │ -2  │  2  │  1  │ -1  │  4  │
+ * └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
+ *           Maximum subarray is [4, -2, 2, 1, -1, 4]
+ * Example 2:
+ * Input: nums = [-1]
+ * Output: -1
+ * Explanation: Since there's only one element, the maximum subarray is the element itself.
+ * 
+ * 
+ * Constraints:
+ * - 1 <= nums.length <= 1000
+ * - -1000 <= nums[i] <= 1000
+ */
+
+export function maxSubArray(nums: number[]): number {
+    return -1
+}
+
+
