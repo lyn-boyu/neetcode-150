e1220437e8f763ee4b9f7ff8151815715f219b8c
jason-l <linc.3395@gmail.com>
2024-10-27 15:23:26 -0700

    feat: add problem trapping-rain-water

---
+++ 02-two-pointers/trapping-rain-water/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Trapping Rain Water - Hard
+ * https://neetcode.io/problems/trapping-rain-water
+ * 
+ * You are given an array of non-negative integers `heights` representing an elevation map where each element represents the height of a bar with width 1.
+ * 
+ * Return the maximum amount of water that can be trapped between the bars.
+ * 
+ * Example 1:
+ * Input: heights = [0, 2, 0, 3, 1, 0, 1, 3, 2, 1]
+ * Output: 9
+ * 
+ * Constraints:
+ * - 1 <= heights.length <= 1000
+ * - 0 <= heights[i] <= 1000
+ */
+
+export function trap(height: number[]): number {
+    return -1
+}
