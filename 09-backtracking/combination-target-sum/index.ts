9e1badb2459093d57c9f41c01cf59c5ba1de2881
jason-l <linc.3395@gmail.com>
2024-11-06 18:50:53 -0800

    feat: add problem combination-target-sum

---
+++ 09-backtracking/combination-target-sum/index.ts
@@ -0,0 +1,30 @@
+/**
+ * Combination Target Sum - Medium
+ * https://neetcode.io/problems/combination-target-sum
+ * 
+ * Given an array of distinct integers nums and a target integer target,
+ * return a list of all unique combinations of nums where the chosen numbers sum to target.
+ * The same number may be chosen from nums an unlimited number of times.
+ * 
+ * Example 1:
+ * Input: nums = [2,5,6,9], target = 9
+ * Output: [[2,2,5],[9]]
+ * 
+ * Example 2:
+ * Input: nums = [3,4,5], target = 16
+ * Output: [[3,3,3,3,4],[3,3,5,5],[4,4,4,4],[3,4,4,5]]
+ * 
+ * Example 3:
+ * Input: nums = [3], target = 5
+ * Output: []
+ * 
+ * Constraints:
+ * - All elements of nums are distinct.
+ * - 1 <= nums.length <= 20
+ * - 2 <= nums[i] <= 30
+ * - 2 <= target <= 30
+ */
+
+export function combinationSum(nums: number[], target: number): number[][] {
+    return []
+}
