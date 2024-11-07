14c302861c7348f53568b399e194f944bc9bab7b
jason-l <linc.3395@gmail.com>
2024-11-07 13:44:39 -0800

    feat: add problem for n-queens

---
+++ 09-backtracking/n-queens/index.ts
@@ -0,0 +1,22 @@
+/**
+ * N-Queens - Hard
+ * https://neetcode.io/problems/n-queens
+ * 
+ * The n-queens puzzle is the problem of placing n queens on an n x n chessboard 
+ * so that no two queens can attack each other.
+ * 
+ * Example 1:
+ * Input: n = 4
+ * Output: [[".Q..","...Q","Q...","..Q."], ["..Q.","Q...","...Q",".Q.."]]
+ * 
+ * Example 2:
+ * Input: n = 1
+ * Output: [["Q"]]
+ * 
+ * Constraints:
+ * - 1 <= n <= 8
+ */
+
+export function solveNQueens(n: number): string[][] {
+   return []
+}
