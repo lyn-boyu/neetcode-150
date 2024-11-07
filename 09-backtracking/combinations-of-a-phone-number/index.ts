9d48689e991d0320144185309ddedba2f6f38069
jason-l <linc.3395@gmail.com>
2024-11-07 11:37:27 -0800

    feat: add problem combinations-of-a-phone-number

---
+++ 09-backtracking/combinations-of-a-phone-number/index.ts
@@ -0,0 +1,31 @@
+/**
+ * Combinations of a Phone Number - Medium
+ * https://neetcode.io/problems/combinations-of-a-phone-number
+ * 
+ * You are given a string digits made up of digits from 2 through 9 inclusive.
+ * Each digit is mapped to a set of characters as shown below:
+ * 2 -> "abc", 3 -> "def", 4 -> "ghi", 5 -> "jkl",
+ * 6 -> "mno", 7 -> "pqrs", 8 -> "tuv", 9 -> "wxyz"
+ * 
+ * Return all possible letter combinations that digits could represent.
+ * You may return the answer in any order.
+ * 
+ * Example 1:
+ * Input: digits = "34"
+ * Output: ["dg", "dh", "di", "eg", "eh", "ei", "fg", "fh", "fi"]
+ * Explanation: The combinations are formed using the characters mapped to 3 ("d", "e", "f")
+ * and 4 ("g", "h", "i").
+ * 
+ * Example 2:
+ * Input: digits = ""
+ * Output: []
+ * Explanation: No digits means no combinations.
+ * 
+ * Constraints:
+ * - 0 <= digits.length <= 4
+ * - 2 <= digits[i] <= 9
+ */
+
+export function letterCombinations(digits: string): string[] {
+    return []
+}
