05e60a809b4f0995824fabddbc25d0daa8b899c6
jason-l <linc.3395@gmail.com>
2024-11-06 23:37:21 -0800

    feat: add problem palindrome-partitioning

---
+++ 09-backtracking/palindrome-partitioning/index.ts
@@ -0,0 +1,30 @@
+/**
+ * Palindrome Partitioning - Medium
+ * https://neetcode.io/problems/palindrome-partitioning
+ * 
+ * Given a string s, split s into substrings where every substring is a palindrome.
+ * Return all possible lists of palindromic substrings.
+ * 
+ * Example 1:
+ * Input: s = "aab"
+ * Output: [["a","a","b"],["aa","b"]]
+ * 
+ * Example 2:
+ * Input: s = "a"
+ * Output: [["a"]]
+ * 
+ * Constraints:
+ * - 1 <= s.length <= 20
+ * - s contains only lowercase English letters.
+ */
+
+// Utility function to check if a substring is a palindrome
+function isPalindrome(str: string, left: number, right: number): boolean {
+    return true;
+}
+
+
+
+export function partition(s: string): string[][] {
+   
+}
