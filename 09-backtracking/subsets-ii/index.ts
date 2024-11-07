fe0a999cf3c367b62ca51ec8996d625de67a1793
jason-l <linc.3395@gmail.com>
2024-11-07 10:10:04 -0800

    feat: add solution for subsets-ii

--- 09-backtracking/subsets-ii/index.ts
+++ 09-backtracking/subsets-ii/index.ts
@@ -20,5 +20,32 @@
  */
 
 export function subsetsWithDup(nums: number[]): number[][] {
-    return []
+    const result: number[][] = []
+    const subset: number[] = []
+
+    // Step1: sort nums to skip the duplicates
+    nums.sort((a, b) => a - b);
+
+    // Step2: define a helper function for backtracking
+    function backtrack(startIdx: number) {
+        // 2.1 add current set to result
+        result.push([...subset])
+
+        // 2.2 explore further num to form a subset
+        for (let i = startIdx; i < nums.length; i++) {
+            // 2.2.1 skip duplicated num 
+            if (i > startIdx && nums[i] === nums[i - 1]) continue
+
+            // 2.2.2 pick current num and explore next position
+            subset.push(nums[i])
+            backtrack(i + 1)
+
+            // 2.2.3 cancel previous selection
+            subset.pop()
+        }
+    }
+
+    // Step 3: Start backtracking and return the result
+    backtrack(0)
+    return result
 }
