9ad1667a1cce7d632b4eee5c98a174ac51d82f30
jason-l <linc.3395@gmail.com>
2024-11-09 11:34:26 -0800

    feat: add problem valid-tree

---
+++ 11-graphs/valid-tree/index.ts
@@ -0,0 +1,39 @@
+/**
+ * Valid Tree - Medium
+ * https://neetcode.io/problems/valid-tree
+ * 
+ * Given `n` nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes),
+ * write a function to check whether these edges make up a valid tree.
+ * 
+ * Example 1:
+ * Input:
+ * n = 5
+ * edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
+ * 
+ * Output:
+ * true
+ * 
+ * Explanation:
+ * - This graph is connected and does not contain any cycles.
+ * 
+ * Example 2:
+ * Input:
+ * n = 5
+ * edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]
+ * 
+ * Output:
+ * false
+ * 
+ * Explanation:
+ * - This graph contains a cycle, so it is not a tree.
+ * 
+ * Constraints:
+ * - 1 <= n <= 100
+ * - 0 <= edges.length <= n * (n - 1) / 2
+ */
+
+export function validTree(n: number, edges: number[][]): boolean {
+    return false
+}
+
+
