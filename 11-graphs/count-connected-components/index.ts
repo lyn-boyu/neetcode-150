89543d0b3d79b4ceddf8fab9487d6b291caec81d
jason-l <linc.3395@gmail.com>
2024-11-09 13:14:34 -0800

    fear: add problem count-connected-components

---
+++ 11-graphs/count-connected-components/index.ts
@@ -0,0 +1,38 @@
+/**
+ * Count Connected Components - Medium
+ * https://neetcode.io/problems/count-connected-components
+ * 
+ * Given an undirected graph with `n` nodes labeled from `0` to `n - 1` and an array `edges`,
+ * where `edges[i] = [a, b]` represents an edge between node `a` and node `b`,
+ * return the total number of connected components in the graph.
+ * 
+ * Example 1:
+ * Input:
+ * n = 3
+ * edges = [[0, 1], [0, 2]]
+ * 
+ * Output:
+ * 1
+ * 
+ * Explanation:
+ * - All nodes are connected, so there is only one connected component.
+ * 
+ * Example 2:
+ * Input:
+ * n = 6
+ * edges = [[0, 1], [1, 2], [2, 3], [4, 5]]
+ * 
+ * Output:
+ * 2
+ * 
+ * Explanation:
+ * - There are two connected components: {0, 1, 2, 3} and {4, 5}.
+ * 
+ * Constraints:
+ * - 1 <= n <= 100
+ * - 0 <= edges.length <= n * (n - 1) / 2
+ */
+
+export function countComponents(n: number, edges: number[][]): number {
+    return -1
+}
