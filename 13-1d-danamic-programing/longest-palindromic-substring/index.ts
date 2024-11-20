ba4bf1a5dd13b4df3aa1644aec75f36de3dbeb57
jason-l <linc.3395@gmail.com>
2024-11-20 13:19:32 -0800

    feat: add solution for longest-palindromic-substring

--- 13-1d-danamic-programing/longest-palindromic-substring/index.ts
+++ 13-1d-danamic-programing/longest-palindromic-substring/index.ts
@@ -18,5 +18,40 @@
  */
 
 export function longestPalindrome(s: string): string {
-    return ''
+    const n = s.length
+
+    // Step 1: Initialize DP table
+    const dp = Array.from({ length: n }, () => Array(n).fill(false))
+    /** 
+        Table Content:
+        * dp[l][r] represents whether the substring from index `l` to `r` is a palindrome.
+        
+        Boolean Explanation:
+        * dp[l][r] = true: The substring s[l...r] is a palindrome.
+        * dp[l][r] = false: The substring s[l...r] is not a palindrome.
+    */
+
+    let startIdx = 0; //Starting index of the longest palindrome
+    let maxLen = 0; // Length of the longest palindrome
+
+    // Step 2: Populate the DP table
+    for (let l = n - 1; l >= 0; l--) {
+        for (let r = l; r < n; r++) {
+            // Check if substring s[l..r] is a palindrome
+            // case1: 'a' r - l = 1
+            // case2: 'aa' r - l = 2
+            // case3:  r - l > 2  and s[r] = s[l] and dp[r + 1][l - 1] is palindrome
+            if (s[l] === s[r] && r - l <= 2 || dp[l + 1][r - 1]) {
+                dp[l][r] = true
+
+                // Update the longest palindrome if needed
+                if (r - l + 1 > maxLen) {
+                    startIdx = l;
+                    maxLen = r - l + 1
+                }
+            }
+        }
+    }
+    // Step 3: Return the longest palindrome substring
+    return s.substring(startIdx, startIdx + maxLen)
 }
