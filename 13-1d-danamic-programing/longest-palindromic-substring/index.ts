09265fc7dd2717bee3c3652d6a62f906c5f0fe1b
jason-l <linc.3395@gmail.com>
2024-11-20 00:14:44 -0800

    feat: add problem longest-palindromic-substring

---
+++ 13-1d-danamic-programing/longest-palindromic-substring/index.ts
@@ -0,0 +1,22 @@
+/**
+ * Longest Palindromic Substring - Medium
+ * https://neetcode.io/problems/longest-palindromic-substring
+ *
+ * Given a string `s`, return the longest substring of `s` that is a palindrome.
+ *
+ * Example 1:
+ * Input: s = "ababd"
+ * Output: "bab"
+ *
+ * Example 2:
+ * Input: s = "abbc"
+ * Output: "bb"
+ *
+ * Constraints:
+ * 1 <= s.length <= 1000
+ * s contains only digits and English letters.
+ */
+
+export function longestPalindrome(s: string): string {
+    return ''
+}
