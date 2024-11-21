ef8bd3dd5f2db952a071a766ec5717e40cf1427a
jason-l <linc.3395@gmail.com>
2024-11-20 17:58:32 -0800

    feat: add problem for palindromic-substrings

---
+++ 13-1d-danamic-programing/palindromic-substrings/index.ts
@@ -0,0 +1,30 @@
+/**
+ * Palindromic Substrings - Medium
+ * https://neetcode.io/problems/palindromic-substrings
+ *
+ * Given a string `s`, return the number of substrings within `s` that are palindromes.
+ *
+ * A palindrome is a string that reads the same forward and backward.
+ *
+ * Example 1:
+ * Input: s = "abc"
+ * Output: 3
+ * Explanation: The substrings are "a", "b", "c".
+ *
+ * Example 2:
+ * Input: s = "aaa"
+ * Output: 6
+ * Explanation: The substrings are "a", "a", "a", "aa", "aa", "aaa".
+ * Note: Different substrings are counted as different palindromes even if they have the same content.
+ *
+ * Constraints:
+ * 1 <= s.length <= 1000
+ * s consists of lowercase English letters.
+ */
+
+export function countSubstrings(s: string): number {
+    return -1
+}
+
+
+ 
